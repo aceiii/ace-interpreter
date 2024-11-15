addb8cc34215059a840db51803fbd07a2ed8bd9d
Borin Ouch <borin.ouch@gmail.com>
2024-11-14 21:12:01 -0800

    Update generate AST tool to implement visitor pattern

--- jlox/src/tool/GenerateAst.java
+++ jlox/src/tool/GenerateAst.java
@@ -30,6 +30,8 @@ public class GenerateAst {
         writer.println();
         writer.println("public abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         // AST classes
         for (String type : types) {
             String className = type.split(":")[0].trim();
@@ -37,8 +39,24 @@ public class GenerateAst {
             defineType(writer, baseName, className, fields);
         }
 
+        // base accept() method
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
+    }
+
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
     }
 
     private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -57,7 +75,14 @@ public class GenerateAst {
 
         writer.println("        }");
 
-        // Field
+        // Visitor pattern
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
+
+        // Fields
         writer.println();
         for (String field: fields) {
             writer.println("        final " + field + ";");
