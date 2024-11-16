716abcd4a854d4152a18ec51a36b80a4b9430a4a
Borin Ouch <borin.ouch@gmail.com>
2024-11-15 18:35:29 -0800

    Implement initial parsing and error handling

--- jlox/src/lox/AstPrinter.java
+++ jlox/src/lox/AstPrinter.java
@@ -29,7 +29,7 @@ public class AstPrinter implements Expr.Visitor<String> {
     }
 
     private String parenthesize(String name, Expr... exprs) {
-        StringBuilder builder =new StringBuilder();
+        StringBuilder builder = new StringBuilder();
 
         builder.append("(").append(name);
         for (Expr expr : exprs) {
