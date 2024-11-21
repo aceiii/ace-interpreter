893c01e0b5b834d9996318a01bcfe207b825ec48
Borin Ouch <borin.ouch@gmail.com>
2024-11-21 01:04:13 -0800

    Add static analysis for resolving local variable binding

--- jlox/src/lox/Environment.java
+++ jlox/src/lox/Environment.java
@@ -27,6 +27,10 @@ public class Environment {
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
+    public Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
     public void define(String name, Object value) {
         values.put(name, value);
     }
@@ -43,5 +47,17 @@ public class Environment {
         }
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+    }
+
+    public void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
+    }
+
+    private Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+        return environment;
     }
 }
