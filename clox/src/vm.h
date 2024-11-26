81cbcffdf322bb2dda242767e3639aed4703e978
Borin Ouch <borin.ouch@gmail.com>
2024-11-26 02:43:04 -0800

    Implement string interning

--- clox/src/vm.h
+++ clox/src/vm.h
@@ -3,6 +3,7 @@
 
 
 #include "chunk.h"
+#include "table.h"
 #include "value.h"
 
 
@@ -14,6 +15,7 @@ typedef struct {
     uint8_t* ip;
     Value stack[STACK_MAX];
     Value* stackTop;
+    Table strings;
     Obj* objects;
 } VM;
 
