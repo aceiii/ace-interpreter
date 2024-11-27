23c7c0cfb7b905caac91c5e22a0cf72e4c0660e8
Borin Ouch <borin.ouch@gmail.com>
2024-11-27 11:07:57 -0800

    Add expression statement

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -282,6 +282,12 @@ static void expression() {
     parsePrecendence(PREC_ASSIGNMENT);
 }
 
+static void expressionStatement() {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
+    emitByte(OP_POP);
+}
+
 static void printStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -295,6 +301,8 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else {
+        expressionStatement();
     }
 }
 
