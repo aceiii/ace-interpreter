b0a422d05e56c78ed1a38e65520e875b44b7be5f
Borin Ouch <borin.ouch@gmail.com>
2024-11-30 01:31:18 -0800

    Add prep work for hoisting captured upvalues

--- clox/src/object.h
+++ clox/src/object.h
@@ -26,6 +26,7 @@ typedef enum {
     OBJ_FUNCTION,
     OBJ_NATIVE,
     OBJ_STRING,
+    OBJ_UPVALUE,
 } ObjType;
 
 
@@ -61,9 +62,17 @@ struct ObjString {
 };
 
 
+typedef struct ObjUpvalue {
+    Obj obj;
+    Value* location;
+} ObjUpvalue;
+
+
 typedef struct {
     Obj obj;
     ObjFunction* function;
+    ObjUpvalue** upvalues;
+    int upvalueCount;
 } ObjClosure;
 
 
@@ -72,6 +81,7 @@ ObjFunction* newFunction();
 ObjNative* newNative(NativeFn function);
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
+ObjUpvalue* newUpvalue(Value* slot);
 void printObject(Value value);
 
 
